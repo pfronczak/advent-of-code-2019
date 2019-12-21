a1f993468ced5660a10702c3ba9ed3caa6a938c5
Paweł Fronczak <pfronczak@gmail.com>
2019-12-21 12:51:11 +0100

    day 9

---
+++ day9/day9-2.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const Computer = require('./computer.js');
+
+const rl = readline.createInterface({
+    input: process.stdin
+});
+
+
+let codeStr = '';
+rl.on('line', (input) => {
+    codeStr += input;
+}).on('close', () => {
+    const code = codeStr.split(',').map(c => parseInt(c));
+    let computer = new Computer(code.slice());
+    computer.input.push(2);
+    computer.run();
+    console.log(computer.output);
+});
