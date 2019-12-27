03e5bf91485bfb8bcf537b0c3d5d84a169ad6c9e
pawelfronczak <pawel.fronczak@cloudity.pl>
2019-12-27 15:47:46 +0100

    day 11, part 1

---
+++ day11/computer.js
@@ -0,0 +1,132 @@
+module.exports = class Computer {
+    constructor(mem, onInput, onOutput) {
+        this.ip = 0;
+        this.relativeBase = 0;
+        this.onInput = onInput;
+        this.onOutput = onOutput;
+        this.done = false;
+        this.mem = mem;
+    }
+
+    run() {
+        let opcode;
+        do {
+            let instrCode = this.mem[this.ip];
+            opcode = instrCode % 100;
+            let instruction = instructions[opcode];
+            if (instruction) {
+                const paramModes = [2, 3, 4].map(pow => Math.floor(instrCode / Math.pow(10, pow)) % 10);
+                this.ip = instruction.execute(this, this.mem.slice(this.ip + 1, this.ip + instruction.length), paramModes);
+            } else {
+                throw new Error(`Invalid op: ${opcode}`);
+            }
+        } while (opcode != 99);
+        if (opcode == 99) {
+            this.done = true;
+        }
+    }
+}
+
+const instructions = {
+    1: {    // add
+        length: 4,
+        execute: (computer, params, paramModes) => {
+            let arg1 = getArgValue(params[0], paramModes[0], computer);
+            let arg2 = getArgValue(params[1], paramModes[1], computer);
+            setMemValue(params[2], paramModes[2], arg1 + arg2, computer);
+            return computer.ip + 4;
+        }
+    },
+    2: {    // multiply
+        length: 4,
+        execute: (computer, params, paramModes) => {
+            let arg1 = getArgValue(params[0], paramModes[0], computer);
+            let arg2 = getArgValue(params[1], paramModes[1], computer);
+            setMemValue(params[2], paramModes[2], arg1 * arg2, computer);
+            return computer.ip + 4;
+        }
+    },
+    3: {    // input
+        length: 2,
+        execute: (computer, params, paramModes) => {
+            setMemValue(params[0], paramModes[0], computer.onInput(), computer);
+            return computer.ip + 2;
+        }
+    },
+    4: {    // output
+        length: 2,
+        execute: (computer, params, paramModes) => {
+            let arg1 = getArgValue(params[0], paramModes[0], computer);
+            // console.log(arg1);
+            computer.onOutput(arg1);
+            return computer.ip + 2;
+        }
+    },
+    5: {    // jump-if-true
+        length: 3,
+        execute: (computer, params, paramModes) => {
+            let arg1 = getArgValue(params[0], paramModes[0], computer);
+            let arg2 = getArgValue(params[1], paramModes[1], computer);
+            return arg1 != 0 ? arg2 : computer.ip + 3;
+        }
+    },
+    6: {    // jump-if-false
+        length: 3,
+        execute: (computer, params, paramModes) => {
+            let arg1 = getArgValue(params[0], paramModes[0], computer);
+            let arg2 = getArgValue(params[1], paramModes[1], computer);
+            return arg1 == 0 ? arg2 : computer.ip + 3;
+        }
+    },
+    7: {    // less-than
+        length: 4,
+        execute: (computer, params, paramModes) => {
+            let arg1 = getArgValue(params[0], paramModes[0], computer);
+            let arg2 = getArgValue(params[1], paramModes[1], computer);
+            setMemValue(params[2], paramModes[2], arg1 < arg2 ? 1 : 0, computer);
+            return computer.ip + 4;
+        }
+    },
+    8: {    // equals
+        length: 4,
+        execute: (computer, params, paramModes) => {
+            let arg1 = getArgValue(params[0], paramModes[0], computer);
+            let arg2 = getArgValue(params[1], paramModes[1], computer);
+            setMemValue(params[2], paramModes[2], arg1 == arg2 ? 1 : 0, computer);
+            return computer.ip + 4;
+        }
+    },
+    9: {    // relative base offset
+        length: 2,
+        execute: (computer, params, paramModes) => {
+            let arg1 = getArgValue(params[0], paramModes[0], computer);
+            computer.relativeBase += arg1;
+            return computer.ip + 2;
+        }
+    },
+    99: {
+        length: 1,
+        execute: (computer) => computer.ip + 1
+    }
+}
+
+function getArgValue(param, paramMode, { mem, relativeBase }) {
+    if (paramMode == 0) {   // position mode
+        return mem[param] || 0;
+    } else if (paramMode == 1) {    // immediate mode
+        return param;
+    } else if (paramMode == 2) {    // relative mode
+        return mem[relativeBase + param] || 0;
+    } else {
+        throw new Error(`Invalid parameter mode: ${paramMode}`);
+    }
+    return null;
+}
+
+function setMemValue(param, paramMode, value, { mem, relativeBase }) {
+    if (paramMode == 0) {
+        mem[param] = value;
+    } else if (paramMode == 2) {
+        mem[param + relativeBase] = value;                
+    }
+}
---
+++ day11/day11-1.js
@@ -0,0 +1,72 @@
+const readline = require('readline');
+const Computer = require('./computer.js');
+
+const rl = readline.createInterface({
+    input: process.stdin
+});
+
+const paintedGrid = new Map();
+
+let codeStr = '';
+rl.on('line', (input) => {
+    codeStr += input;
+}).on('close', () => {
+    const code = codeStr.split(',').map(c => parseInt(c));
+    let x = 0, y = 0, dir = 0;
+    let paint = true;
+    let computer = new Computer(code.slice(),
+        () => onInput(`${x},${y}`),
+        (val) => {
+            if (paint) {
+                paintedGrid.set(`${x},${y}`, val);
+                paint = false;
+            } else {
+                if (val == 1) {
+                    dir = (dir + 1) % 4;
+                } else if (val == 0) {
+                    dir = (dir + 4 - 1) % 4;
+                }
+                [x, y] = move[dir](x, y);
+                paint = true;
+            }
+        });
+    computer.run();
+    console.log(paintedGrid.size);
+    printGrid(paintedGrid);
+});
+
+function onInput(currentPos) {
+    if (paintedGrid.has(currentPos)) {
+        return paintedGrid.get(currentPos);
+    }
+    return 0;
+}
+
+const move = {
+    0: (x,y) => [ x, y - 1 ],
+    1: (x,y) => [ x + 1, y ],
+    2: (x,y) => [ x, y + 1 ],
+    3: (x,y) => [ x - 1, y ],
+}
+
+function printGrid(grid) {
+    let minX = Infinity, maxX = -Infinity;
+    let minY = Infinity, maxY = -Infinity;
+    for (pos of grid.keys()) {
+        let [x, y] = pos.split(',');
+        if (x < minX) minX = x;
+        if (x > maxX) maxX = x;
+        if (y < minY) minY = y;
+        if (y > maxY) maxY = y;
+    }
+    for (y = minY; y <= maxY; y++) {
+        for (x = minX; x <= maxX; x++) {
+            if (grid.has(`${x},${y}`) && grid.get(`${x},${y}`) == 1) {
+                process.stdout.write('#');
+            } else {
+                process.stdout.write(' ');
+            }
+        }
+        process.stdout.write('\n');
+    }
+}
